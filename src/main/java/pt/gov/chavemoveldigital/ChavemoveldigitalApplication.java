26ad6d88029c94ad55b563c825e765e1d7524caa
Alexandre Ardigo <alexaardigo@gmail.com>
2025-06-21 14:53:33 +0100

    initialize project with Spring Boot setup and Maven wrapper

---
+++ src/main/java/pt/gov/chavemoveldigital/ChavemoveldigitalApplication.java
@@ -0,0 +1,13 @@
+package pt.gov.chavemoveldigital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChavemoveldigitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChavemoveldigitalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pt/gov/chavemoveldigital/ChavemoveldigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.gov.chavemoveldigital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChavemoveldigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
