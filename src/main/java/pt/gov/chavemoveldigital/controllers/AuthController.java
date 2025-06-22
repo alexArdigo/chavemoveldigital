0970092492707aa918c82b04a6dcb7c92bf846ec
Alexandre Ardigo <alexaardigo@gmail.com>
2025-06-22 11:12:18 +0100

    Add initial implementation of AuthController for authentication endpoints

---
+++ src/main/java/pt/gov/chavemoveldigital/controllers/AuthController.java
@@ -0,0 +1,24 @@
+package pt.gov.chavemoveldigital.controllers;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthController {
+
+    @Autowired
+    private AuthService authService;
+
+    @PostMapping("/authentication")
+    public ResponseEntity<?> authenticate(String telephoneNumber) {
+        return ResponseEntity.ok().body(authService.authenticate(telephoneNumber));
+    }
+
+    @PostMapping("/authentication/code")
+    public ResponseEntity<?> insertCode(Long code) {
+        return ResponseEntity.ok().body(authService.insertCode(code));
+    }
+}
