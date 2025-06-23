f0af2e2b495414fb23dc11cf65a1e5477024aa63
Alexandre Ardigo <alexaardigo@gmail.com>
2025-06-23 19:55:49 +0100

    Refactor electoral circle and parish to municipality in user data

--- src/main/java/pt/gov/chavemoveldigital/enums/ElectoralCircle.java
+++ src/main/java/pt/gov/chavemoveldigital/enums/ElectoralCircle.java
@@ -1,4 +1,43 @@
 package pt.gov.chavemoveldigital.enums;
 
 public enum ElectoralCircle {
+    VIANA_DO_CASTELO,
+    BRAGA,
+    VILA_REAL,
+    BRAGANCA,
+    PORTO,
+    AVEIRO,
+    VISEU,
+    GUARDA,
+
+    // Portugal Continental - Centro
+    COIMBRA,
+    LEIRIA,
+    CASTELO_BRANCO,
+
+    // Portugal Continental - Lisboa e Vale do Tejo
+    SANTAREM,
+    LISBOA,
+    PORTALEGRE,
+
+    // Portugal Continental - Alentejo
+    EVORA,
+    BEJA,
+
+    // Portugal Continental - Algarve
+    FARO,
+
+    // Área Metropolitana de Lisboa
+    SETUBAL,
+
+    // Regiões Autónomas
+    MADEIRA,
+    ACORES,
+
+    //Círculos da Emigração
+    EUROPA,
+    RESTO_DO_MUNDO,
+
+    ElectoralCircle() {
+    }
 }
